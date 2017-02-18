cdf2f3953949b381dbde4eae40cbac947c6bfdc3
Øivind Stensrud <oivind@oivind.net>
2017-02-18 23:25:56 +0100

    New operation: toggleState
    Some minor refactoring.

--- src/main/java/net/oivind/java/HueCLI/parser/CLIParser.java
+++ src/main/java/net/oivind/java/HueCLI/parser/CLIParser.java
@@ -11,21 +11,34 @@ public class CLIParser {
     private HelpFormatter formatter = new HelpFormatter();
     private CommandHandler ch = new CommandHandler();
 
+    private final String SHOW_ALL = "show-all";
+    private final String INFORMATION = "information";
+    private final String TOGGLE = "toggle";
+
     public CLIParser() {
         addOptions();
     }
 
     private void addOptions() {
+
         options.addOption(Option.builder("s")
                 .argName("s")
-                .longOpt("show-all")
+                .longOpt(SHOW_ALL)
                 .desc("Show all lights")
                 .build());
 
-        options.addOption(Option.builder("l")
-                .longOpt("light")
-                .desc("Show info about one light")
+        options.addOption(Option.builder("i")
+                .longOpt(INFORMATION)
+                .desc("Show information about one light or one group")
                 .hasArg()
+                .build());
+
+
+        options.addOption(Option.builder("t")
+                .longOpt(TOGGLE)
+                .desc("Turn light on or off")
+                .hasArg()
+                .numberOfArgs(2)
                 .build());
 
     }
@@ -33,12 +46,16 @@ public class CLIParser {
     public void doParse(String[] args) {
         try {
             CommandLine cmd = parser.parse(options, args);
-            if (cmd.hasOption("s")) {
+            if (cmd.hasOption(SHOW_ALL)) {
                 System.out.println("Show all lights");
                 ch.showAllLights();
-            } else if (cmd.hasOption("l")) {
+            } else if (cmd.hasOption(INFORMATION)) {
                 System.out.println("Show one light");
                 ch.showOneLight(1);
+            } else if (cmd.hasOption(TOGGLE)) {
+                System.out.println("Toggle light");
+                final String[] optionValues = cmd.getOptionValues(TOGGLE);
+                ch.toggleState(Integer.parseInt(optionValues[0]), Boolean.valueOf(optionValues[1]));
             } else {
                 showHelp();
             }
