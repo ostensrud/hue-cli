310554eb8d2ebe6375bd40b79e95881210e93dbc
Øivind Stensrud <oivind@oivind.net>
2017-02-20 20:59:29 +0100

    Pretty print of all lights and their state

--- src/main/java/net/oivind/java/HueCLI/core/CommandHandler.java
+++ src/main/java/net/oivind/java/HueCLI/core/CommandHandler.java
@@ -1,18 +1,35 @@
 package net.oivind.java.HueCLI.core;
 
+import net.oivind.java.HueCLI.DataTypes.Light;
 import net.oivind.java.HueCLI.Net.HttpHandler;
 import net.oivind.java.HueCLI.properties.PropertiesReader;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.StringJoiner;
 
 public class CommandHandler {
     private HttpHandler httph = new HttpHandler();
     private PropertiesReader pr = new PropertiesReader();
     private HueOperations hueOp = new HueOperations();
+    private JSONHelper jsonHelper = new JSONHelper();
 
     public void showAllLights() throws IOException {
-        System.out.println(httph.doGet(buildUrl() + "/" + hueOp.getPath("default")));
+        System.out.println();
+        Map<String, Light> lights = jsonHelper.mapJsonToObject(httph.doGet(buildUrl() + "/" + hueOp.getPath("default")), Light.gsonType);
+        StringBuilder sb = new StringBuilder();
+
+        int counter = 1;
+        for (String key : lights.keySet()) {
+            sb.append(counter++)
+                    .append(" : ")
+                    .append(lights.get(key).getName())
+                    .append(" [")
+                    .append(lights.get(key).getState().isOn())
+                    .append("]\n");
+        }
+
+        System.out.print(sb.toString());
     }
 
     public void showOneLight(int lightNumber) throws IOException {
@@ -20,8 +37,8 @@ public class CommandHandler {
     }
 
     public void toggleState(int lightNumber, boolean turnOn) throws IOException {
-        String json = "{\"on\":"+Boolean.toString(turnOn)+"}";
-        System.out.println(httph.doPut(buildUrl() + "/" + hueOp.getPath("default") + "/" + Integer.toString(lightNumber)+"/"+hueOp.getPath("change_state"),json));
+        String json = "{\"on\":" + Boolean.toString(turnOn) + "}";
+        System.out.println(httph.doPut(buildUrl() + "/" + hueOp.getPath("default") + "/" + Integer.toString(lightNumber) + "/" + hueOp.getPath("change_state"), json));
     }
 
     private String buildUrl() {
