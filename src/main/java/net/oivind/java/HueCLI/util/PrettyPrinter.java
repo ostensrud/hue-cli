b392f6c09b8e5ea679a720383bf44749b9025d27
Øivind Stensrud <oivind@oivind.net>
2017-03-12 20:21:06 +0100

    Colors!
    Only tested in urxvt.

--- src/main/java/net/oivind/java/HueCLI/util/PrettyPrinter.java
+++ src/main/java/net/oivind/java/HueCLI/util/PrettyPrinter.java
@@ -5,6 +5,19 @@ import net.oivind.java.HueCLI.DataTypes.Light;
 import java.util.Map;
 
 public class PrettyPrinter {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BOLD = "\u001B[1m";
+
+
     public static void printAllLights(Map<String, Light> lights) {
         StringBuilder sb = new StringBuilder();
 
@@ -49,6 +62,6 @@ public class PrettyPrinter {
     }
 
     public static String getOnOff(boolean state) {
-        return state ? "on" : "off";
+        return state ? ANSI_GREEN+"on"+ANSI_RESET : ANSI_RED+"off"+ANSI_RESET;
     }
 }
